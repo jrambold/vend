da78fb6086b2dd731baf4f31c34e1f8f4421f05f
jrambold <jrambold@gmail.com>
2018-04-06 10:45:27 -0600

    added 1st 2 user stories

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,3 +1,12 @@
 class Machine < ApplicationRecord
   belongs_to :owner
+  has_many :machine_snacks
+
+  #yes there's an activerecord way to do that better but i couldn't think of it quickly
+  def avg_price
+    total = machine_snacks.to_a.sum do |ms|
+      ms.snack.price
+    end
+    total / machine_snacks.count
+  end
 end
--- spec/features/user_can_see_vending_machines_spec.rb
+++ spec/features/user_can_see_vending_machines_spec.rb
@@ -12,3 +12,33 @@ feature "When a user visits the vending machine index" do
     expect(page).to have_content("Turing Basement")
   end
 end
+
+feature "When a user visits a vending machine page" do
+  scenario "they see a list of snacks with price associated with that vending machine" do
+    sam = Owner.create(name: "Sam's Snacks")
+    don = sam.machines.create(location: "Don's Mixed Drinks")
+    snack = Snack.create(name: 'Snickers', price: 5)
+    snack2 = Snack.create(name: 'kitkat', price: 15)
+    don.machine_snacks.create!(snack_id: snack.id)
+    don.machine_snacks.create!(snack_id: snack2.id)
+
+    visit machine_path(don)
+
+    expect(page).to have_content(snack.name)
+    expect(page).to have_content(snack.price)
+    expect(page).to have_content(snack2.name)
+    expect(page).to have_content(snack2.price)
+  end
+  scenario "they see the average price of a snack" do
+    sam = Owner.create(name: "Sam's Snacks")
+    don = sam.machines.create(location: "Don's Mixed Drinks")
+    snack = Snack.create(name: 'Snickers', price: 5)
+    snack2 = Snack.create(name: 'kitkat', price: 15)
+    don.machine_snacks.create!(snack_id: snack.id)
+    don.machine_snacks.create!(snack_id: snack2.id)
+
+    visit machine_path(don)
+
+    expect(page).to have_content(10)
+  end
+end
